f61ca4aaaf6c46c98fc24f8bec7863090d3c812c
Sahithi Karise <ksahithi9615@gmail.com>
2025-08-24 22:38:30 +0530

    Update credit_card_model.py

--- credit_card_model.py
+++ credit_card_model.py
@@ -1,44 +1,3 @@
-''' ORIGINAL CODE'''
-# import pandas as pd
-# import numpy as np
-# from sklearn.model_selection import train_test_split
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
-# from imblearn.over_sampling import SMOTE
-
-# # Load data
-# df = pd.read_csv('creditcard.csv')  # from Kaggle
-
-# # Separate features and target
-# X = df.drop('Class', axis=1)
-# y = df['Class']
-
-# # Train-test split
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
-
-# # Feature scaling
-# scaler = StandardScaler()
-# X_train_scaled = scaler.fit_transform(X_train)
-# X_test_scaled = scaler.transform(X_test)
-
-# # Handle imbalance with SMOTE
-# sm = SMOTE(random_state=42)
-# X_train_res, y_train_res = sm.fit_resample(X_train_scaled, y_train)
-
-# # Train model
-# model = RandomForestClassifier()
-# model.fit(X_train_res, y_train_res)
-
-# # Predict
-# y_pred = model.predict(X_test_scaled)
-
-# # Evaluate
-# print(confusion_matrix(y_test, y_pred))
-# print(classification_report(y_test, y_pred))
-# print("ROC AUC Score:", roc_auc_score(y_test, model.predict_proba(X_test_scaled)[:, 1]))
-
-''' REDUCED THE SIZE OF THE DATASET FOR REDUCING THE EXECUTION TIME'''
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -80,4 +39,5 @@ y_pred = model.predict(X_test_scaled)
 # Evaluate
 print("Confusion Matrix\n",confusion_matrix(y_test, y_pred))
 print("Classification Report\n",classification_report(y_test, y_pred))
+
 print("ROC AUC Score:", roc_auc_score(y_test, model.predict_proba(X_test_scaled)[:, 1]))
